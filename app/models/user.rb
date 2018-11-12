f5cd07cfae6e09d36de66c3de74d182a1c9a0f7e
Linda ONeill <oneill.linda21@gmail.com>
2018-11-12 16:53:42 -0600

    remove confirmable from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :items
 end
